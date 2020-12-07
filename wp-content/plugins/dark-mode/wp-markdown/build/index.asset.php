b7b5d5624eac98d57a249abf4e5e037cec00d447
yuliataroiktkhk <julia.taro@khk.ee>
2020-12-08 00:49:03 +0200

    modified plugin `WP Markdown Editor (Formerly Dark Mode)` version 4.0.1 (after activation of `WP Markdown Editor (Formerly Dark Mode)` version 4.0.1)

---
+++ wp-content/plugins/dark-mode/wp-markdown/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-keycodes', 'wp-plugins', 'wp-polyfill', 'wp-rich-text', 'wp-url', 'wp-wordcount'), 'version' => 'd3b9ac51084b1d9022213a8c34924008');
