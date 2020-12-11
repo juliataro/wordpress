88e881c049fb190f5f1894ac6e0bb299ce2bd9db
yuliataroiktkhk <julia.taro@khk.ee>
2020-12-11 12:52:00 +0200

    modified file `error_log`
    	Conflict: wp-content/wflogs/config-livewaf.php [local version]

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1608288715;s:7:"authKey";s:64:"<j~PrI#S/,!kF*pP.OmV9KJ3m:M49M:ADjnyL5JDU0[JxtIp~zE^RC<kx]J<Q)8;";s:7:"version";s:5:"1.0.4";}
+a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1608288715;s:7:"authKey";s:64:"<j~PrI#S/,!kF*pP.OmV9KJ3m:M49M:ADjnyL5JDU0[JxtIp~zE^RC<kx]J<Q)8;";}
