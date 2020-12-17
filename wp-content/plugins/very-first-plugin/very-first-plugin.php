79ffcbea0ffa7779b469b42087e00867b759a8da
yuliataroiktkhk class <julia.taro@khk.ee>
2020-12-17 09:44:31 +0200

    modified plugin `My First plugin` version 1.0 (after activation of `Google Analytics for WordPress by MonsterInsights` version 7.14.0)

---
+++ wp-content/plugins/very-first-plugin/very-first-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: My First plugin
+Plugin URI: http://yuliataro.ikt.khk.ee/
+Description: This is my first WordPress Plugin i ever created
+Version: 1.0
+Author: Julia Taro
+Author URI: http://yuliataro.ikt.khk.ee/
+
+*/
