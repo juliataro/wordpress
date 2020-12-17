65068610c5ecc08a678a2aeaa708766f3f285007
yuliataroiktkhk class <julia.taro@khk.ee>
2020-12-17 23:39:01 +0200

    modified plugin `My First plugin` version 1.0 (after deactivation of `My First plugin` version 1.0)

--- wp-content/plugins/very-first-plugin/very-first-plugin.php
+++ wp-content/plugins/very-first-plugin/very-first-plugin.php
@@ -7,4 +7,10 @@ Version: 1.0
 Author: Julia Taro
 Author URI: http://yuliataro.ikt.khk.ee/
 
-*/
+**/
+function dh_modify_read_more_link() {
+    return '<a class="more-link" href="' . get_permalink() . '">Click to Read!</a>'; 
+    
+}
+add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
+ 
