fa20468b2356bd6f09283e3548c9b33634aed15d
yuliataroiktkhk class <julia.taro@khk.ee>
2020-12-18 17:44:28 +0200

    modified plugin `My First plugin` version 1.0 (after activation of `Valentine's Day Hearts` version 2.0)

--- wp-content/plugins/very-first-plugin/very-first-plugin.php
+++ wp-content/plugins/very-first-plugin/very-first-plugin.php
@@ -9,5 +9,9 @@ Author URI: http://yuliataro.ikt.khk.ee/
 
 **/
 
-
+function dh_modify_read_more_link() {
+    return '<a class="more-link" href="' . get_permalink() . '">Tuli välja!</a>'; 
+    
+}
+add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
  
